e19620e8f59b8304c1a0c65d4b327cbeeff8d0ff
Ayman Mahmoud <57979775+ayman-m@users.noreply.github.com>
2020-12-09 17:44:02 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 'use strict';
 
+var async = require('async');
+var _ = require('underscore');
 var exec = require('child_process').exec;
+var request = require('request');
+var uuid = require('node-uuid');
+var fs = require('fs-extra');
 var extractBearerToken =
     require('../common/extractBearerToken.js');
+
 _downloadArchive("https://google.com", extractBearerToken, function () {
 })
 
 function _downloadArchive(x, z, next) {
-    const command = `curl -o "${x}" "${z}"`
+    const command = `curl -o ${x} "${z}"`
     exec(command,
         function (err) {
             if (err)
                 return next(
-                    new ActErr('zzz')
                 );
 
             return next();
