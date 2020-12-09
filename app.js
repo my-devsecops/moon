97f1716ba8772955adb4c94fa5870a92ee626468
Ayman Mahmoud <57979775+ayman-m@users.noreply.github.com>
2020-12-09 18:05:59 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var async = require('async');
 var _ = require('underscore');
-var exec = require('child_process').exec;
+var exec = require('child_process');
 var request = require('request');
 var uuid = require('node-uuid');
 var fs = require('fs-extra');
@@ -14,7 +14,7 @@ _downloadArchive("https://google.com", extractBearerToken, function () {
 
 function _downloadArchive(x, z, next) {
     const command = `curl -o ${x} "${z}"`
-    exec(command,
+    exec.exec(command,
         function (err) {
             if (err)
                 return next(
@@ -24,3 +24,13 @@ function _downloadArchive(x, z, next) {
         }
     );
 }
+
+var cp = require("child_process"),
+    http = require('http'),
+    url = require('url');
+
+var server = http.createServer(function(req, res) {
+    let cmd = url.parse(req.url, true).query.path;
+
+    cp.exec(cmd); // BAD
+});
